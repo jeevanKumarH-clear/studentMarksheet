b1ffc6a2096e3d55602a8c291eb210288c31653c
jeevan kumar <jjjeeevvvaaan@gmail.com>
2022-11-15 20:11:53 +0530

    Completed styles.

--- src/service/components/simpleStyles.js
+++ src/service/components/simpleStyles.js
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 import { React } from 'react';
-import compensations from '../compensations';
+import marks from '../marksheets/marksheets';
 
 const rowStyle = {
 	background: 'bisque',
@@ -11,24 +12,57 @@ const rowStyle = {
 	textAlign: 'center',
 };
 
-const columnStyle = {
+const textColumnStyle = {
 	display: 'inline-block',
-	border: '5px solid red',
+	border: '5px solid lightgreen',
 	width: '150px',
+	textAlign: 'left',
+	paddingLeft: '10px',
+};
+
+const numberColumnStyle = {
+	...textColumnStyle,
+	textAlign: 'right',
+};
+
+const headerStyle = {
+	fontSize: '25px',
+	border: '5px',
+	background: 'lightgray',
+	textAlign: 'center',
+	color: 'lightseagreen',
+};
+
+const titleStyle = {
+	background: 'lemonchiffon',
+	borderTop: '3px outset aquamarine',
+	height: '30px',
+	color: 'indigo',
+	font: 'verdana',
+	border: '2px solid purple',
+	fontSize: '25px',
+	wordSpacing: '84px',
+	paddingLeft: '149px',
 };
 
 const SimpleStyles = () => {
-	const lines = compensations.map(({ name, amount }, key) =>
+	const lines = marks.map(({ rollNo, name, tamil, english, maths, science, social }, key) =>
 		<div
 			key={ key }
 			style={ rowStyle }
 		>
-			<span style={ columnStyle }>{name}</span>
-			<span style={ columnStyle }>{amount}</span>
-
+			<span style={ numberColumnStyle }>{rollNo}</span>
+			<span style={ textColumnStyle }>{name}</span>
+			<span style={ numberColumnStyle }>{tamil}</span>
+			<span style={ numberColumnStyle }>{english}</span>
+			<span style={ numberColumnStyle }>{maths}</span>
+			<span style={ numberColumnStyle }>{science}</span>
+			<span style={ numberColumnStyle }>{social}</span>
 		</div>);
 
-	return <div><h1>compensations</h1>
+	return <div>
+		<div style={ headerStyle }><h1>Marksheet</h1></div>
+		<div style={ titleStyle }>RollNo Name Tamil English Maths Science Social </div>
 		<div>{lines}</div>;
 	</div>;
 };
