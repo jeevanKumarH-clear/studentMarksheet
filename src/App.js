cc131392485f8afde6214ff091ccbd23edc2e951
jeevan kumar <jjjeeevvvaaan@gmail.com>
2022-11-16 20:11:24 +0530

    Changed styles to classname.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { React } from 'react';
 import './App.scss';
 import SimpleTable from './service/components/simpleTable';
 
-const App = () => <SimpleTable/>;
+const App = () =>
+	<div className="App" role="App">
+		<SimpleTable/>;
+	</div>;
 
 export default App;
--- src/service/components/simpleTable.js
+++ src/service/components/simpleTable.js
@@ -1,56 +1,48 @@
-/* eslint-disable max-len */
-/* eslint-disable react/jsx-indent */
 /* eslint-disable max-lines-per-function */
 import { React } from 'react';
 import marks from '../marksheets/marksheets';
 
-const tableStyle = {
-	border: '5px dotted cornflowerblue',
-	width: '200px',
-	margin: 'auto',
-};
-
-const rowStyle = {
-	border: '2px solid black',
-	width: '100px',
-	background: 'mistyrose',
-};
-
-const headerStyle = {
-	fontSize: '25px',
-	border: '5px',
-	background: 'lightgray',
-	textAlign: 'center',
-	color: 'cornflowerblue',
-};
-
-// eslint-disable-next-line max-len
-const headings = ['RollNo', 'Name', 'Tamil', 'English', 'Maths', 'Science', 'Social', 'Total'];
+const headings = [
+	'RollNo',
+	'Name',
+	'Tamil',
+	'English',
+	'Maths',
+	'Science',
+	'Social',
+	'Total',
+];
 
 const SimpleTable = () =>
 	<div>
-		<h1>Basic Table</h1>
-		<table style={ tableStyle }><thead>
-			<tr>
-				{headings.map((details, index) =>
-					<th key={ index } style={ headerStyle }>{details}</th>)}
-			</tr>
-		</thead>
-		<tbody>
-			{
-				marks.map((student, key) =>
-					<tr key={ key }>
-						<td style={ rowStyle }>{student.rollNo}</td>
-						<td style={ rowStyle }>{student.name}</td>
-						<td style={ rowStyle }>{student.tamil}</td>
-						<td style={ rowStyle }>{student.english}</td>
-						<td style={ rowStyle }>{student.maths}</td>
-						<td style={ rowStyle }>{student.science}</td>
-						<td style={ rowStyle }>{student.social}</td>
-						<td style={ rowStyle }>{student.tamil + student.english + student.maths + student.science + student.social}</td>
-					</tr>)
-			}
-		</tbody>
+		<h1>Marksheet</h1>
+		<table className="tableStyle">
+			<thead>
+				<tr>
+					{headings.map((details, index) =>
+						// eslint-disable-next-line max-len
+						<th key={ index } className="headerStyle">{details}</th>)}
+				</tr>
+			</thead>
+			<tbody>
+				{
+					marks.map((student, key) =>
+						<tr key={ key }>
+							<td className="rowStyle">{student.rollNo}</td>
+							<td className="rowStyle">{student.name}</td>
+							<td className="rowStyle">{student.tamil}</td>
+							<td className="rowStyle">{student.english}</td>
+							<td className="rowStyle">{student.maths}</td>
+							<td className="rowStyle">{student.science}</td>
+							<td className="rowStyle">{student.social}</td>
+							<td className="rowStyle">{student.tamil
+								+ student.english
+								+ student.maths
+								+ student.science
+								+ student.social}</td>
+						</tr>)
+				}
+			</tbody>
 		</table>
 	</div>;
 
